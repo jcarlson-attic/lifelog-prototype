98f40419d9e7b0fcefd32e3f8a987e6897ba569b
Jarrod Carlson <jarrod.carlson@gmail.com>
2008-02-07 04:16:01 +0000

    Entry Type management is pretty much complete

--- app/models/entry_type.rb
+++ app/models/entry_type.rb
@@ -1,4 +1,5 @@
 class EntryType < ActiveRecord::Base
-  has_many :entry_type_attribute_type
+  has_many :entry_type_attribute_types
+  has_many :attribute_types
   validates_presence_of :name, :message => "is required!"
 end
